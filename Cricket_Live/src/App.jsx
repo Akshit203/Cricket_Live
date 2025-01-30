8b974895fe8df53bce33bd59faf12f4b0f143223
ayush9951 <ayushgupta9951@gmail.com>
2025-01-31 00:54:54 +0530

    Test commit

--- Cricket_Live/src/App.jsx
+++ Cricket_Live/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <>
-        <div className='text-7xl'>Cricket Live - INDIA</div>
-        <div className='text-7xl'>Cricket Live - INDIA</div>
+      <div className="text-7xl">Cricket Live - INDIA</div>
+      <div className="text-7xl">Cricket Live - INDIA</div>
+      <div className="text-2xl mt-5 border p-5 text-center">
+        Test Successful
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
