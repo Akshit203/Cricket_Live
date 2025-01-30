781ce550e8b290aaef74b039adcd4e328d47c33f
Akshit203 <akshitgupta2003.ak@gmail.com>
2025-01-31 00:34:40 +0530

    .

--- Cricket_Live/src/App.jsx
+++ Cricket_Live/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-7xl'>Cricket Live</div>
+    <>
+        <div className='text-7xl'>Cricket Live - INDIA</div>
+        <div className='text-7xl'>Cricket Live - INDIA</div>
+    </>
+
   )
 }
 
